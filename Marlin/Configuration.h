a6cfc8c6bedd40a95270fbf1854d98104b784b6c
Can Bal <canbal@gmail.com>
2021-05-26 21:21:06 -0700

    PID autotune params

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -606,9 +606,12 @@
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
     // MP3DP Titan Aero
-    #define DEFAULT_Kp 7.5
-    #define DEFAULT_Ki 0.5
-    #define DEFAULT_Kd 125
+    // #define DEFAULT_Kp 7.5
+    // #define DEFAULT_Ki 0.5
+    // #define DEFAULT_Kd 125
+    #define DEFAULT_Kp 13.05
+    #define DEFAULT_Ki 0.60
+    #define DEFAULT_Kd 70.50
 
   #endif
 #endif // PIDTEMP
@@ -648,9 +651,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 51.31
+  #define DEFAULT_bedKi 9.97
+  #define DEFAULT_bedKd 175.98
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
