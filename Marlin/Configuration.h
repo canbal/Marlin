bd19b97ec534413beb12b575c59a4484b1febd5c
Can Bal <canbal@gmail.com>
2021-09-16 23:00:09 -0700

    adjusting z offset after a fresh printed attachment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1060,7 +1060,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 25, -2.7 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 25, -3.2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
